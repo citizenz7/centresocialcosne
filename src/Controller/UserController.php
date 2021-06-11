da39f99db6856365c2736490d611f337961cabe7
citizenz7 <citizenz7@protonmail.com>
2021-06-11 17:05:16 +0200

    fix session suppression d'un compte

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
@@ -130,6 +131,10 @@ class UserController extends AbstractController
             $entityManager->remove($user);
             $entityManager->flush();
         }
+
+        // fix : destroy session after deleting account
+        $session = new Session();
+        $session->invalidate();
 
         return $this->redirectToRoute('app_login');
     }
